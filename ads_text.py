bf4bec3a8ef769bbfe14e6bfc07c78462ca5338f
RodRojas <rojaslee@gmail.com>
2016-06-17 20:35:26 -0400

    Add files via upload

---
+++ ads_text.py
@@ -0,0 +1,79 @@
+## this file would contain dictionaries for each item, in this case musical instruments, and the keys in the dictionaries correspond to the fields in the form. This is a couple of sample dictionaries below:
+
+
+
+
+china_violin = {
+    'price_amount': 90 ,
+    'ad_title': "Violin, excellent sound, sold by violin instructor" ,
+    'description': """
+
+<<<----Please click the video on the left to listen the violin 
+- full size 4/4 Light red colour : $90 
+- fullsize 4/4 Dark brown colour : $100 
+- 1/2 and 3/4 size Dark brown :$100 
+- Intermediate Violin full size 4/4 Yellow brown colour : $300 (breathtaking sound, could be used even for professionals)
+
+
+- four fine tuners 
+- lightweight hard case included 
+- wooden bow included 
+- rosin included 
+- wooden pegs 
+- alloy tailpiece 
+-overall, very well made instrument. 
+
+""" ,
+
+    'postal_code': "M6P 1A6" ,
+    'street_address': "1585 Bloor Street West, Toronto, ON" ,
+    'youtube': "https://www.youtube.com/watch?v=scDv59kGMcU" ,
+}
+
+cello = {
+    'price_amount': '499' ,
+    'ad_title':'CELLO from $499, solid wood' ,
+    'description': """
+<<<<<<-------Please click on the left to watch video of this cello. 
+
+Cellos sold by cello Instructor 
+
+-New Cello : $499 
+-Used Cello : $450 
+-Advanced Cello : $2000 
+
+CELLO, solid spruce top, solid maple back, full size (4/4), with bow and soft case. 
+
+cello has an beautiful singing sound, due to the nice woods that were used to make it. 
+-The cello comes with a free soft case. 
+-if you want a specific bridge height, I can cater to your needs 
+-long term customer service (discounted or free maintenance) 
+-you can get one free cello lesson with purchase 
+-click on the left video to hear the sound of these instruments 
+-you can come and see the instrument with no obligation 
+-if you do not play the cello, I can play it for you so that you hear it 
+
+I also teach cello, if you are interested
+
+
+""" ,
+
+    'postal_code': "M6P 1A6" ,
+    'street_address': "1585 Bloor Street West, Toronto, ON" ,
+    'youtube': 'https://www.youtube.com/watch?v=iTj1BLfKFUc' ,
+}
+
+
+# this one of the lists that will be iterated by the program, pulling out the dictionaries
+    
+
+instrument_list = [  china_violin, cello ]
+
+
+
+    
+    
+    
+    
+
+	
